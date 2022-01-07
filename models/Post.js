2eb5e7372e3899febec4945f3bab4e04fad8c414
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-01-06 19:51:53 -0700

    models

---
+++ models/Post.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+
+}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        contents: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        created_by: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        date: {
+            type: DataTypes.DATE
+        },
+        //think about how to add comment 
+        comment: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
